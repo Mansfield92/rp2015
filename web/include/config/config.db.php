6d80326b8f2af6930081a9cfae59fcde530c844a
Petr Vomáčko <p.vomacko@gmail.com>
2015-10-12 22:38:15 +0200

    Train Detail

---
+++ web/include/config/config.db.php
@@ -0,0 +1,8 @@
+<?php
+#localhost
+$sql_login_host = "localhost"; # MySQL Host
+$sql_login_user = "root"; # MySql UserName
+$sql_login_pass = ""; # MySql Password
+$sql_login_db = "rockinovy_projekt"; # MySql Database
+
+$con = mysqli_connect($sql_login_host, $sql_login_user, $sql_login_pass, $sql_login_db);
