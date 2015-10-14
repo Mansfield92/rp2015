6a0748a6ab00053a95f08a6dc428451ced06c477
Petr Vomáčko <petr.vomacko@symbiodigital.com>
2015-10-14 21:30:42 +0200

    train list

--- web/include/config/config.db.php
+++ web/include/config/config.db.php
@@ -3,6 +3,6 @@
 $sql_login_host = "localhost"; # MySQL Host
 $sql_login_user = "root"; # MySql UserName
 $sql_login_pass = ""; # MySql Password
-$sql_login_db = "rockinovy_projekt"; # MySql Database
+$sql_login_db = "rocnikovy_projekt"; # MySql Database
 
 $con = mysqli_connect($sql_login_host, $sql_login_user, $sql_login_pass, $sql_login_db);
